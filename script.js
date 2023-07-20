f1244c04fa1c4272057a604f9570d0d4cf713c66
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-21 00:19:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let namexx = prompt('Type here');
+
+
+let taskobj={};
+let count=1;
+for(let i=0;i<namexx.length;i++){
+    if(taskobj.hasOwnProperty(namexx[i])){
+        taskobj[namexx[i]]++;
+    }else{
+        taskobj[namexx[i]]=count;
+    }
+}
+console.log(taskobj);
+for(let i=0;i<namexx.length;i++){
+    if(taskobj[namexx[i]]===1){
+        console.log(namexx[i]);
+        let bar = confirm(namexx[i]);
+		
+     break;
+    }
+}
+console.log(namexx, bar);
